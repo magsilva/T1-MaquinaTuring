41efd0b5b815ef5edced501f425e53ca7edf4e02
vmichelan <venanciuslm@gmail.com>
2017-11-30 18:06:41 -0200

    Reestruturado tape e turing machine

--- tape.py
+++ tape.py
@@ -6,6 +6,13 @@ class tape:
         self.content = content
         self.size = len(content)
 
+
+    def move_head(self, movement):
+        if movement == 'L':
+            self.move_left()
+        elif movement == 'R':
+            self.move_right()
+
     def move_left(self):
         if self.position > 0:
             self.position -= 1
@@ -13,9 +20,15 @@ class tape:
             self.content.insert(0,self.whitespace_symbol)
 
     def move_right(self):
-        if self.position < len(self.content):
+        if self.position < len(self.content)-1:
             self.position += 1
         else:
             whitespace = self.whitespace_symbol
             self.content.append(whitespace)
             self.position += 1
+
+    def get_content(self):
+        return self.content[self.position]
+
+    def set_content(self, symbol):
+        self.content[self.position] = symbol 
--- turing_machine.py
+++ turing_machine.py
@@ -12,44 +12,19 @@ class turing_machine:
         self.whitespace = whitespace_symbol
         self.current_state = first_state
 
-    def move_head(self, tape_number, movement):
-        if movement == 'L':
-
-            self.tape_list[tape_number].move_left()
-        elif movement == 'R':
-
-            self.tape_list[tape_number].move_right()
 
     def run(self):
         self.tape_list[0].size = len(self.tape_list[0].content)
         while self.step():
             ''' just a empty while function that executes turing machine's steps'''
 
-
-    def getElementAtPosition(self, position, tapeElement):
-
-        if position >= 0 and position < tapeElement.size:
-            return tapeElement.content[position]
-        elif  position < 0:
-            tapeElement.size += 1
-             
-            tapeElement.content.insert(0,self.whitespace)
-
-        else:
-            tapeElement.size += 1
-            tapeElement.content.append(self.whitespace)
-        return self.whitespace
-
-
     def step(self):
         for transition in self.transitions:
-            head = int(self.tape_list[0].position)
- 
-            if int(self.current_state) == int(transition[0]) and self.getElementAtPosition(head, self.tape_list[0]) == transition[2]:
+            if int(self.current_state) == int(transition[0]) and self.tape_list[0].get_content() == transition[2]:
                
-                self.tape_list[0].content[head] = transition[3]
+                self.tape_list[0].set_content(transition[3])
                 self.current_state = transition[1]
-                self.move_head(0,transition[4])
+                self.tape_list[0].move_head(transition[4])
                
                   
                 return 1
