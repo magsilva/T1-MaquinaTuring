5e29512d726b680e8e482434cb21c20a74cc7656
Marco Aurélio Graciotto Silva <magsilva@utfpr.edu.br>
2019-04-01 11:19:09 -0300

    Add verification of acceptance.

--- turing_machine.py
+++ turing_machine.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 from instance  import instance
 import copy
 ''' @mod turing_machine: módulo que representa uma turing machine '''
@@ -35,23 +38,33 @@ class turing_machine:
 
         
         # Laço de repeticao que executa enquanto existir instancias de maquina de turing
-        
         while self.instances:
             instances = self.instances # para nao iterar em uma lista que tem possibilidade de nao modificacao
-            # iterar sobre a lista de instâncias
+
+            # Realiza transicoes
             for instance in instances:
+                # Verifica aceitacao por estado final
+                for final_state in self.final_states:
+                    # Se o estado atual é igual ao estado final
+                    if instance.current_state == final_state:
+                        # término da execucao da turing machine
+                        return [1, instance]
+
                 # stepResult recebe a lista de transições válidas
-                stepResult = instance.step(self.transitions)   
+                stepResult = instance.step(self.transitions) 
+
                 # verifica se não tem transição válida
                 if len(stepResult) == 0:
                     self.instances.remove(instance)
                 else:
-                # realiza a primeira transição na instância que esta iterando
+                    # realiza a primeira transição na instância que esta iterando
                     instance.doTransition(stepResult[0])
-                # deleta a transição executada
+
+                    # deleta a transição executada
                     del stepResult[0]
-                # realiza cópias da instância e executa as transições restantes nelas    
+
+                    # realiza cópias da instância e executa as transições restantes nelas    
                     for result in stepResult:
                         self.instances.append(copy.deepcopy(instance)) 
                         self.instances[-1].doTransition(result)
-        print(False)
+        return [0, None]
