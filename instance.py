1b8b995c54680541263346e16334b33d56a1ef89
Marco Aurélio Graciotto Silva <magsilva@utfpr.edu.br>
2019-04-01 11:18:01 -0300

    Fix reading of data. Remove acceptance verification. Add str method.

--- instance.py
+++ instance.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 from tape import tape
 
 
@@ -14,44 +17,36 @@ class instance:
         @func doTransision: realiza uma transição
         @param transition: transição a ser executada pela instância
     '''
-    def doTransition(self,transition):
+    def doTransition(self, transition):
         self.current_state = transition[1]
         # usado para acessar o símbolo da transição para aquela fita
-        tapeIndex = 1
+        tapeIndex = 0
 
         # executa a transição para todas as fitas da instância
         for tape in self.tape_list:
             # modifica o conteúdo da posição atual da fita
-            tape.set_content(transition[3*(tapeIndex)])
+            tape.set_content(transition[3 + (4*tapeIndex)])
             # move a cabeça da fita 
-            tape.move_head(transition[(3*tapeIndex)+1])
+            tape.move_head(transition[4 + (4*tapeIndex)])
             tapeIndex += 1
 
-        # Verifica se o estado atual está na lista de estados finais
-        for final_state in self.final_states:
-            # Se o estado atual é igual ao estado final
-            if self.current_state == final_state:
-            # término da execucao da turing machine
-                print(True)
-                exit(0)
-        return 0
 
     '''
         @func step: Verifica as transições válidas para a instância da máquina de turing
     '''
-    def step(self,transitions):
+    def step(self, transitions):
         validTransitions = []
         for transition in transitions:
             # Verifica se o estado atual da instância  é igual ao estado de partida da transição
-            if int(self.current_state) == int(transition[0]): 
+            if int(self.current_state) == int(transition[0]):
                 #contador de transições válidas de fitas que aceitam a transição
                 validTapeTransitions = 0
                 # usado para acessar o símbolo da transicao para aquela fita
-                tapeIndex = 1
+                tapeIndex = 0
                 # itera sobre a lista de fitas da instância
                 for tape in self.tape_list: 
                     # se o conteudo da transicao é igual ao conteúdo atual da fita, conta-se uma fita válida
-                    if tape.get_content() == transition[(3*tapeIndex)-1]:
+                    if tape.get_content() == transition[2 + (4 * tapeIndex)]:
                         validTapeTransitions += 1
                     # caso contrário, pare a iteração sobre a lista de fitas (tem que ser válido para todas as fitas)
                     else:
@@ -64,3 +59,11 @@ class instance:
                     validTransitions.append(transition)
 
         return validTransitions
+
+    def __str__(self):
+        result = "["
+        for tape in self.tape_list:
+            result += str(tape)
+            result += ","
+        result += "]"
+        return result
